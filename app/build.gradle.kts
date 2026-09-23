c62e76a902e1a2fc1562ccb699f675cbb583df27
Pallav Srivastava <pallav619@gmail.com>
2026-09-23 18:13:14 +0530

    build(app): bump version to 1.0.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,8 @@ android {
         applicationId = "com.draw.onme"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
